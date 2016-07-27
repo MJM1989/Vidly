9ce7ea6f005d16ea490a3ffae283e308ad345476
MJM1989 <info@martinmohlmann.nl>
2016-07-27 09:47:23 +0200

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // We can construct custom routes this way, but it is a bit messy, because the strings are not updated easily
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate", 
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}"});
+
+            // This is a cleaner way, which we combine with the [Route] attribute in the controller:
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                // This sets the default actions to take on entering an url:
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
